2adf3dd5816d1a17ae5c8a2bf7c33e866c5feee0
Zackary Liel <47030040+zliel@users.noreply.github.com>
2024-09-24 16:43:00 -0700

    Added a "Home" example page to demonstrate router usage

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import Typography from '@mui/material/Typography'
+import { Box } from '@mui/material'
+
+export const Home = () => {
+  return (
+    <Box sx={{ p: 2 }} justifyContent={"center"}>
+      <Typography textAlign={"center"} variant={"h1"}>Home</Typography>
+    </Box>
+  )
+}
