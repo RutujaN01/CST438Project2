a3ed410aa40ce0bd933a5c620bbbe21eb4bfd8cb
rutujanigade <rnigade@csumb.edu>
2024-09-30 20:33:49 -0700

    Created landing page for TechMart

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import { Routes, Route } from 'react-router-dom'
-import { Home }from './pages/Home'
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage'; 
+import LandingPage from './pages/LandingPage'; 
 
 export default function App() {
   const routes = [
-    { path: "/", Component: <Home />},
-    ]
+    { path: "/", Component: <LandingPage /> }, 
+    { path: "/home", Component: <HomePage /> }, 
+  ];
+
   return (
     <Routes>
       {routes.map(({ path, Component }) => (
-        <Route path={path} element={Component} />
-        ))
-      }
+        <Route key={path} path={path} element={Component} />
+      ))}
     </Routes>
-  )
+  );
 }
